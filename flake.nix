9325417e26f82a1eb5ad30b38feed7f7af62fa80
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-16 12:18:18 -0700

    Update

--- flake.nix
+++ flake.nix
@@ -22,15 +22,25 @@
 
         shellHook = ''
           VENV=.venv
+
+          # Create venv if it doesn't exist
           if [ ! -d "$VENV" ]; then
-            echo "Creating new venv..."
+            echo "Creating new venv at $VENV..."
             python -m venv "$VENV"
           fi
-
+          
+          # Enter the venv
           source "$VENV/bin/activate"
           
           # Fix for C-libraries in pip-installed packages
           export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib:$LD_LIBRARY_PATH"
+
+          # Install the packages in a requirements.txt if there is one
+          if [ -d "requirements.txt" ]; then
+            echo "Found requirements.txt, running pip install..."
+            python -m pip install -r requirements.txt
+            echo "Requirements installed!"
+          fi
           
           echo "Python virtual environment activated!"
           python --version
