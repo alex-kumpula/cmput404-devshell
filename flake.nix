a69496528f78f47b639cf29f6565a558dd6a761c
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-16 00:02:48 -0700

    test

--- flake.nix
+++ flake.nix
@@ -2,29 +2,25 @@
   description = "A simple Python development environment";
 
   inputs = {
-    # Points to the stable NixOS package repository
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs }:
     let
-      # Systems to support
-      system = "x86_64-linux"; # Use "aarch64-darwin" for Apple Silicon Macs
+      system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        # Packages to include in the shell
         buildInputs = [
-          (pkgs.python3.withPackages (python-pkgs: [
+          (pkgs.python311.withPackages (python-pkgs: [
             python-pkgs.pandas
             python-pkgs.requests
           ]))
         ];
 
-        # Commands to run when the shell starts
         shellHook = ''
-          echo "Welcome to your Python dev environment!"
+          echo "Entered a Python development shell!"
           python --version
         '';
       };
